3b795c9cf5c95315a35d1fd2a24e178114bf6318
Paulo Andreola <pauloandreola@gmail.com>
2023-03-21 09:00:28 -0300

    feat: fix and update all in-memory file with all methods included

--- src/repositories/tasks/in-memory.ts
+++ src/repositories/tasks/in-memory.ts
@@ -1,11 +1,13 @@
 import { randomUUID } from "crypto";
 import { Task } from "../../entities/Task";
-import { TasksRepository } from "./interface";
+import { TaskInput, TasksRepository } from "./interface";
 
 export class InMemoryTasksRepository implements TasksRepository {
+
+
   private tasks: Task[] =  [];
 
-  async create(task: Pick<Task, "title" | "description" | "effort">): Promise<Task> {
+  async create(task: TaskInput): Promise<Task> {
     const newTask = {
       ...task,
       id: randomUUID(),
@@ -16,5 +18,34 @@ export class InMemoryTasksRepository implements TasksRepository {
     this.tasks.push(newTask);
 
     return newTask;
+  };
+
+  async deleteById(id: string): Promise<void> {
+    const taskIndex = this.tasks.findIndex((task) => task.id === id);
+
+    this.tasks.splice(taskIndex, 1);
+  };
+
+  async findAll(): Promise<Task[]> {
+    return this.tasks;
   }
-}
+
+  async findById(id: string): Promise<Task | null> {
+    const task = this.tasks.find((task) => task.id === id);
+
+    return task || null;
+  };
+
+  async updateById(id: string, task: Partial<TaskInput>): Promise<Task> {
+    const taskIndex = this.tasks.findIndex(task => task.id === id);
+
+    const updatedTask = {
+      ...this.tasks[taskIndex],
+      ...task,
+    };
+
+    this.tasks[taskIndex] = updatedTask;
+
+    return updatedTask;
+  };
+};
