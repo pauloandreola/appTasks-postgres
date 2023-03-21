203c9027a8bb16278a697d9158a9ff7f98068df4
Paulo Andreola <pauloandreola@gmail.com>
2023-03-21 09:14:06 -0300

    feat: fix and update all interface file with all methods included and task interface created

--- src/repositories/tasks/interface.ts
+++ src/repositories/tasks/interface.ts
@@ -1,5 +1,16 @@
 import { Task } from "../../entities/Task";
 
+export interface TaskInput {
+  title: string;
+  description: string;
+  effort: number;
+  completeAt?: Date;
+}
 export interface TasksRepository {
-  create(task: Pick<Task, 'title' | 'description' | 'effort'>): Promise<Task>
+
+  create(task: TaskInput): Promise<Task>;
+  deleteById(id: string): Promise<void>;
+  findAll(): Promise<Task[]>;
+  findById(id: string): Promise<Task | null>;
+  updateById(id: string, task: Partial<TaskInput>): Promise<Task>;
 }
