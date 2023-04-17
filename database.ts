cc4262c1029ae44f3fe74d65f12955e70de0427e
Paulo Andreola <pauloandreola@gmail.com>
2023-04-17 12:14:15 -0300

    feat: included .env method in database connection function in database file

--- database.ts
+++ database.ts
@@ -1,14 +1,24 @@
 
 import pg from 'pg';
+import dotenv from "dotenv"
+
 const { Client } = pg;
+
+dotenv.config()
+
+const db_host = process.env.DB_HOST;
+const db_port = process.env.DB_PORT as unknown as number;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+const db_database = process.env.DB_DATABASE;
 
 export async function database(query: string) {
   const client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'apptasks',
-    password: 'apptasks',
-    database: 'apptasks',
+    host: db_host,
+    port: db_port,
+    user: db_user,
+    password: db_password,
+    database: db_database,
   })
   await client.connect();
 
